f58bd21865cbc2ea271c6b6eee49f0bb80a71024
Sönke Lange <soenkelange@aol.com>
2016-12-29 17:53:36 +0100

    Add RecyclerView to RecorderFragment to list available Sensors

--- app/src/main/java/de/haw_hamburg/sensorapp/recorder/RecorderFragment.java
+++ app/src/main/java/de/haw_hamburg/sensorapp/recorder/RecorderFragment.java
@@ -1,5 +1,10 @@
 package de.haw_hamburg.sensorapp.recorder;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 import de.haw_hamburg.sensorapp.BaseMvpFragment;
 import de.haw_hamburg.sensorapp.R;
 
@@ -9,12 +14,20 @@ import de.haw_hamburg.sensorapp.R;
  */
 public class RecorderFragment extends BaseMvpFragment<RecorderPresenter, RecorderView> implements RecorderView {
 
+    private RecyclerView sensorsRecyclerView;
+
     public RecorderFragment() {
         // Required empty public constructor
     }
 
     public static RecorderFragment newInstance() {
         return new RecorderFragment();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sensorsRecyclerView = (RecyclerView) view.findViewById(R.id.sensorsRecyclerView);
     }
 
     @Override
