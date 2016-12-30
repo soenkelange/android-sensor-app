8f6eeab3d216cfd882f57f2f9d141b6720c3a8af
Sönke Lange <soenkelange@aol.com>
2016-12-30 21:23:58 +0100

    Change visibility of PresenterViewBinder class

--- app/src/main/java/de/haw_hamburg/sensorapp/mvp/PresenterViewBinder.java
+++ app/src/main/java/de/haw_hamburg/sensorapp/mvp/PresenterViewBinder.java
@@ -4,26 +4,26 @@ package de.haw_hamburg.sensorapp.mvp;
  * Created by s.lange on 27.12.16.
  */
 
-public class PresenterViewBinder<P extends Presenter<V>, V extends View> {
+class PresenterViewBinder<P extends Presenter<V>, V extends View> {
     private final PresenterProvider<P> presenterProvider;
     private final ViewProvider<V> viewProvider;
     private P presenter;
 
-    public PresenterViewBinder(PresenterProvider<P> presenterProvider, ViewProvider<V> viewProvider) {
+    PresenterViewBinder(PresenterProvider<P> presenterProvider, ViewProvider<V> viewProvider) {
         this.presenterProvider = presenterProvider;
         this.viewProvider = viewProvider;
     }
 
-    public void attachView() {
+    void attachView() {
         presenter = presenterProvider.providePresenter();
         presenter.attachView(viewProvider.provideView());
     }
 
-    public void detachView() {
+    void detachView() {
         presenter.detachView();
     }
 
-    public P getPresenter() {
+    P getPresenter() {
         return presenter;
     }
 }
