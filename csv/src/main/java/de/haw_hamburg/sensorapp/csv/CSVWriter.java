852897a9a23084dce356023125a53ad5309f51b3
Sönke Lange <soenkelange@aol.com>
2017-01-07 15:46:28 +0100

    Add CSVWriter

--- csv/src/main/java/de/haw_hamburg/sensorapp/csv/CSVWriter.java
+++ csv/src/main/java/de/haw_hamburg/sensorapp/csv/CSVWriter.java
@@ -1,4 +1,47 @@
 package de.haw_hamburg.sensorapp.csv;
 
+import java.io.IOException;
+import java.io.Writer;
+
 public class CSVWriter {
+
+    public static final String DEFAULT_LINE_END = "\n";
+    public static final String DEFAULT_SEPARATOR = ",";
+
+    private final Writer writer;
+    private String lineEnd = DEFAULT_LINE_END;
+    private String separator = DEFAULT_SEPARATOR;
+
+    public CSVWriter(Writer writer) {
+        this.writer = writer;
+    }
+
+    public void writeNextLine(String[] fields) throws IOException {
+        StringBuilder stringAppendable = new StringBuilder();
+        for (int i = 0; i < fields.length; i++) {
+            if (i != 0) {
+                stringAppendable.append(separator);
+            }
+            String field = fields[i];
+            stringAppendable.append(field);
+        }
+        stringAppendable.append(lineEnd);
+        writer.write(stringAppendable.toString());
+    }
+
+    public String getLineEnd() {
+        return lineEnd;
+    }
+
+    public void setLineEnd(String lineEnd) {
+        this.lineEnd = lineEnd;
+    }
+
+    public String getSeparator() {
+        return separator;
+    }
+
+    private void setSeparator(String separator) {
+        this.separator = separator;
+    }
 }
