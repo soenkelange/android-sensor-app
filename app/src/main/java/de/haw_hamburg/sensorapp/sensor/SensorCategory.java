82e51b303e37fa0ca76495cf142c57229fe78c15
Sönke Lange <soenkelange@aol.com>
2017-01-07 21:56:43 +0100

    Add GetSensorBySensorCategoryMap

---
+++ app/src/main/java/de/haw_hamburg/sensorapp/sensor/SensorCategory.java
@@ -0,0 +1,11 @@
+package de.haw_hamburg.sensorapp.sensor;
+
+/**
+ * Created by s.lange on 07.01.17.
+ */
+public enum SensorCategory {
+    UNKNOWN,
+    MOTION_SENSORS,
+    POSITION_SENSORS,
+    ENVIRONMENT_SENSORS
+}
