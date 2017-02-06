e79d529eedd2b9f55fc771b069ff39247283a681
Sönke Lange <s.lange@mytaxi.com>
2017-02-06 23:01:00 +0100

    Make SpiritLevelFragment extend BaseNavigationFragment

---
+++ app/src/main/java/de/haw_hamburg/sensorapp/spiritlevel/SpiritLevelPresenter.java
@@ -0,0 +1,9 @@
+package de.haw_hamburg.sensorapp.spiritlevel;
+
+import de.haw_hamburg.sensorapp.mvp.AbstractPresenter;
+
+/**
+ * Created by s.lange on 06.02.17.
+ */
+public class SpiritLevelPresenter extends AbstractPresenter<SpiritLevelView> {
+}
