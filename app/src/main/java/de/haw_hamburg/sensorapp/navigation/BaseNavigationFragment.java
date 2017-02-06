64c6ed15a1f1f66f496caf4b978cb9b162c450ef
Sönke Lange <s.lange@mytaxi.com>
2017-02-06 23:14:04 +0100

    Fix settings of title

--- app/src/main/java/de/haw_hamburg/sensorapp/navigation/BaseNavigationFragment.java
+++ app/src/main/java/de/haw_hamburg/sensorapp/navigation/BaseNavigationFragment.java
@@ -9,5 +9,11 @@ import de.haw_hamburg.sensorapp.mvp.Presenter;
 
 public abstract class BaseNavigationFragment<P extends Presenter<V>, V extends de.haw_hamburg.sensorapp.mvp.View> extends BaseMvpFragment<P, V> {
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().setTitle(getTitle());
+    }
+
     public abstract String getTitle();
 }
