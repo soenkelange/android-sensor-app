8fd8cc80a9eee6c1ba97805e6f1dd10110761a52
Sönke Lange <s.lange@mytaxi.com>
2017-02-06 22:52:20 +0100

    Make CompassFragment extend BaseNavigationFragment

---
+++ app/src/main/java/de/haw_hamburg/sensorapp/compass/CompassPresenter.java
@@ -0,0 +1,10 @@
+package de.haw_hamburg.sensorapp.compass;
+
+import de.haw_hamburg.sensorapp.mvp.AbstractPresenter;
+
+/**
+ * Created by s.lange on 06.02.17.
+ */
+
+public class CompassPresenter extends AbstractPresenter<CompassPresenterView> {
+}
