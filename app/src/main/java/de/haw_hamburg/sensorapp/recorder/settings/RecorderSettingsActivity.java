db05b7391a714409a88d57b67efd614a112590dd
Sönke Lange <s.lange@mytaxi.com>
2017-02-08 21:48:20 +0100

    Remove fab from RecorderSettingsActivity

--- app/src/main/java/de/haw_hamburg/sensorapp/recorder/settings/RecorderSettingsActivity.java
+++ app/src/main/java/de/haw_hamburg/sensorapp/recorder/settings/RecorderSettingsActivity.java
@@ -25,15 +25,6 @@ public class RecorderSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recorder_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
