55c3b6c5be8fd5e31d0e9425709d4444fbcd1427
Sönke Lange <s.lange@mytaxi.com>
2017-05-11 01:26:17 +0200

    Fix GetEnabledSensorInteractor provider

--- app/src/main/java/de/haw_hamburg/sensorapp/recorder/RecorderModule.java
+++ app/src/main/java/de/haw_hamburg/sensorapp/recorder/RecorderModule.java
@@ -5,6 +5,7 @@ import dagger.Provides;
 import de.haw_hamburg.rxandroidsensor.RxSensorManager;
 import de.haw_hamburg.sensorapp.sensor.SensorEventListenerInteractor;
 import de.haw_hamburg.sensorapp.sensor.SensorPreferences;
+import de.haw_hamburg.sensorapp.sensor.writer.SensorDescriptorsProvider;
 
 /**
  * Created by s.lange on 17.01.17.
@@ -13,8 +14,11 @@ import de.haw_hamburg.sensorapp.sensor.SensorPreferences;
 public class RecorderModule {
 
     @Provides
-    public GetEnabledSensorInteractor provideGetEnabledSensorInteractor(RxSensorManager rxSensorManager, SensorPreferences sensorPreferences) {
-        return new GetEnabledSensorInteractor(rxSensorManager, sensorPreferences);
+    public GetEnabledSensorInteractor provideGetEnabledSensorInteractor(
+            RxSensorManager rxSensorManager,
+            SensorPreferences sensorPreferences,
+            SensorDescriptorsProvider sensorDescriptorsProvider) {
+        return new GetEnabledSensorInteractor(rxSensorManager, sensorPreferences, sensorDescriptorsProvider);
     }
 
     @Provides
